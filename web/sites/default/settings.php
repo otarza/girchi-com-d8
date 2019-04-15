72138e852d692b53eff8e099a885e62078b0c3a7
davidako <davitako777@gmail.com>
2019-04-15 18:28:15 +0400

    Enables config split.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -727,3 +727,5 @@ $config_directories['sync'] = '../config/sync';
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
+
