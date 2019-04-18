cac36071b84eb6194cc42878d212291080e37bca
GGKapanadze <kapanadzeg41@gmail.com>
2019-04-18 13:04:06 +0400

    #54 Remove unnecessary sleep and clear

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,38 +7,29 @@ PMA="http://pma.girchi.docker.localhost"
 
 #colors 
 NONE='\033[00m'
-BOLD='\033[1m'
 GREEN='\033[01;32m'
+BLUE='\033[1;36m'
 
 clear
-echo -e "${BOLD}Starting up containers\n\v${NONE}"
+echo -e "${BLUE}Starting up containers\n\v${NONE}"
 docker-compose pull
 docker-compose up -d --remove-orphans
-sleep 5
-clear
-echo  -e "${BOLD}Running pre-install script\n\v${NONE}"
-sleep 1
+echo -e "\n"    
+echo  -e "${BLUE}Running pre-install script\n\v${NONE}"
 ./scripts/pre-install.sh
-clear
-echo  -e "${BOLD}Running composer json\n\v${NONE}"
-sleep 1
+echo -e "\n"
+echo  -e "${BLUE}Running composer json\n\v${NONE}"
 docker-compose exec php composer install
-sleep 2
-clear
-echo  -e "${BOLD}Installing drupal\n\v${NONE}"
-sleep 1
-docker-compose exec php drush si --existing-config --account-pass=1234  -vvv -y
-sleep 2
-clear
-echo  -e "${BOLD}Importing translations\n\v${NONE}"
-sleep 1
+echo -e "\n"
+echo  -e "${BLUE}Installing drupal\n\v${NONE}"
+docker-compose exec php drush si --existing-config --account-pass=1234  -y -vvv 
+echo -e "\n"
+echo  -e "${BLUE}Importing translations\n\v${NONE}"
 docker-compose exec php drush language-import
-sleep 2
-clear
-echo  -e "${BOLD}Importing config\n\v${NONE}"
-sleep 1
+echo -e "\n"
+echo  -e "${BLUE}Importing config\n\v${NONE}"
 docker-compose exec php drush cim -y -vvv
-sleep 2
+echo -e "\n"
 clear
-echo  -e "${BOLD}Project is ready${NONE} ${RED}\n\vWebsite${NONE}  - " $GREEN$WEBSITE$NONE"${RED}\n\vMailHog${NONE} - " $GREEN$MAILHOG$NONE"${RED}\n\vphpMyAdmin${NONE} - " $GREEN$PMA$NONE"\n"
+echo  -e "${BLUE}D8 Project is ready${NONE} ${RED}\n\vWebsite${NONE}  - " $GREEN$WEBSITE$NONE"${RED}\n\vMailHog${NONE} - " $GREEN$MAILHOG$NONE"${RED}\n\vphpMyAdmin${NONE} - " $GREEN$PMA$NONE"\n"
 sleep 3
