d34df3804cd2d7dc15092f9f33357578c8681b86
ABGEO <takalandzet@gmail.com>
2019-04-23 14:51:18 +0400

    #49 Import config after install.

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,23 +11,29 @@ GREEN='\033[01;32m'
 BLUE='\033[1;36m'
 
 clear
-echo -e "${BLUE}[1/6] Starting up containers...\n\v${NONE}"
+echo -e "${BLUE}[1/8] Stopping containers...\n\v${NONE}"
+make stop
+
+echo -e "${BLUE}[2/8] Starting up containers...\n\v${NONE}"
 make up
 
-echo -e "\n${BLUE}[2/6] Building UI Project...\n\v${NONE}"
+echo -e "\n${BLUE}[3/8] Building UI Project...\n\v${NONE}"
 mkdir -p front
 make build-ui
 
-echo -e "\n${BLUE}[3/6] Running pre-install script...\n\v${NONE}"
+echo -e "\n${BLUE}[4/8] Running pre-install script...\n\v${NONE}"
 ./scripts/pre-install.sh
 
-echo -e "\n${BLUE}[4/6] Running composer install...\n\v${NONE}"
+echo -e "\n${BLUE}[5/8] Running composer install...\n\v${NONE}"
 docker-compose exec php composer install
 
-echo -e "\n${BLUE}[5/6] Installing drupal...\n\v${NONE}"
+echo -e "\n${BLUE}[6/8] Installing drupal...\n\v${NONE}"
 docker-compose exec php drush si --existing-config --account-pass=1234  -y -vvv 
 
-echo -e "\n${BLUE}[6/6] Importing translations...\n\v${NONE}"
-docker-compose exec php drush language-import
+echo -e "\n${BLUE}[7/8] Importing translations...\n\v${NONE}"
+docker-compose exec php drush language-import -y
+
+echo -e "\n${BLUE}[8/8] Importing config...\n\v${NONE}"
+docker-compose exec php drush cim -y -vvv
 
 echo -e "\n${BLUE}Girchi Website is ready${NONE} ${GREEN}\n\vWebsite${NONE}  - " ${GREEN}${WEBSITE}${NONE}"${GREEN}\n\vMailHog${NONE} - " ${GREEN}${MAILHOG}${NONE}"${GREEN}\n\vphpMyAdmin${NONE} - " ${GREEN}${PMA}${NONE}"\n"
