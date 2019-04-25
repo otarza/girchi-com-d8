cddbb22518cb54fd7c154a3a5e75150aa13c2efb
anano <glonti1899@gmail.com>
2019-04-25 14:35:00 +0400

    #53 Create utils module

--- web/modules/custom/girchi_utils/src/Controller/SupportersRegistrationController.php
+++ web/modules/custom/girchi_utils/src/Controller/SupportersRegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\girchi_utils\Controller;
 
+use Drupal;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Form\FormBuilder;
 use Drupal\user\Entity\User;
@@ -54,6 +55,31 @@ class SupportersRegistrationController extends ControllerBase {
 
     $form = $this->entityFormBuilder->getForm(User::create([]),
       'register');
+
+    unset($form['field_personal_id']['widget']['0']['value']['#title']);
+    unset($form['field_first_name']['widget']['0']['value']['#title']);
+    unset($form['field_last_name']['widget']['0']['value']['#title']);
+    unset($form['field_date_of_birth']['widget']['0']['value']['#title']);
+    unset($form['field_phone']['widget']['0']['value']['#title']);
+    unset($form['account']['mail']['#title']);
+    unset($form['account']['mail']['#description']);
+    unset($form['account']['pass']['pass1']['#title']);
+    unset($form['account']['pass']['pass2']['#title']);
+    unset($form['field_ged']['widget']['0']['value']['#title']);
+
+    $form['field_personal_id']['#attributes']['class'][] = 'form-control form-control-lg';
+    $form['field_first_name']['#attributes']['class'][] = 'form-control form-control-lg';
+    $form['field_last_name']['#attributes']['class'][] = 'form-control form-control-lg';
+    $form['field_date_of_birth']['#attributes']['class'][] = 'form-control form-control-lg';
+    $form['field_phone']['#attributes']['class'][] = 'form-control form-control-lg';
+    $form['account']['mail']['#attributes']['class'][] = 'form-control form-control-lg';
+    $form['account']['pass']['pass1']['#attributes']['class'][] = 'form-control form-control-lg';
+    $form['account']['pass']['pass2']['#attributes']['class'][] = 'form-control form-control-lg';
+    $form['field_ged']['#attributes']['class'][] = 'form-control form-control-lg';
+    $form['field_referral']['widget'][0]['target_id']['#default_value'] = Drupal::currentUser()->getAccount();
+    $form['actions']['submit']['#attributes']['class'][] = 'btn btn-lg btn-block btn-warning text-uppercase mt-4';
+
+//    dump($form);
 
     return [
       '#theme' => 'supporter_registration',
