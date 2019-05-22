3d788b8218e722b0a7dbb4b0045ac8af541e0f55
Farna Chilashvili <farnnn.ff@gmail.com>
2019-05-22 13:29:25 +0400

    #146 implement politician button

---
+++ web/modules/custom/girchi_utils/src/Plugin/Block/PoliticianBlock.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\girchi_utils\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'PoliticianBlock' block.
+ *
+ * @Block(
+ *  id = "politician_block",
+ *  admin_label = @Translation("Politician block"),
+ * )
+ */
+class PoliticianBlock extends BlockBase
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build()
+    {
+        $build = [];
+        $build['politician_block']['#markup'] = 'Implement PoliticianBlock.';
+        $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+
+
+        return array(
+            '#theme' => 'politician_block',
+            '#language' => $language,
+        );
+    }
+
+    public function getCacheMaxAge()
+    {
+        // set block cache max age 3 hours and then invalidate.
+        return 10800;
+    }
+}
