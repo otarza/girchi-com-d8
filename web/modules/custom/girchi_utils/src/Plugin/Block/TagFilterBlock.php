c6abfc2d12ec89e88090f2f32d5a09aea6f923ff
IliaNoz <i_nozadze@cu.edu.ge>
2019-05-30 12:58:35 +0400

    #178 #184 Create Tag configuration. (#193)
    * #178 #184 Create Tag configuration.
    
    * #178 #184 Config update.
    
    * #178 #184 Change field name.

--- web/modules/custom/girchi_utils/src/Plugin/Block/TagFilterBlock.php
+++ web/modules/custom/girchi_utils/src/Plugin/Block/TagFilterBlock.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Drupal\girchi_utils\Plugin\Block;
-
+use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Annotation\Translation;
 use Drupal\Core\Block\Annotation\Block;
 use Drupal\Core\Block\BlockBase;
@@ -9,6 +9,8 @@ use Drupal\Core\Cache\Cache;
 use Drupal\node\Entity\Node;
 use Drupal\node\NodeStorage;
 use Drupal;
+use Drupal\taxonomy\Entity\Term;
+
 /**
  * Provides a 'TagFilterBlock' block.
  *
@@ -26,7 +28,35 @@ class  TagFilterBlock extends BlockBase
      */
     public function build()
     {
-        $tags_tree =  Drupal::service('girchi_utils.taxonomy_term_tree')->load('tags');
+        $tags_tree = [];
+        $query = \Drupal::entityQuery('taxonomy_term')
+            ->condition('vid', 'tags')
+            ->condition('field_featured', '1')
+            ->range(0,10)
+            ->condition('status', 1);
+
+        $tids = $query->execute();
+        if(!empty($tids) && count($tids) < 10) {
+            $query = \Drupal::entityQuery('taxonomy_term')
+                ->condition('vid', 'tags')
+                ->condition('tid', $tids, 'NOT IN')
+                ->range(0,10 - count($tids))
+                ->condition('status', 1);
+            $additional_tids = $query->execute();
+            $tids = array_merge($tids, $additional_tids);
+        } elseif (empty($tids)) {
+            $query = \Drupal::entityQuery('taxonomy_term')
+                ->condition('vid', 'tags')
+                ->range(0,10)
+                ->condition('status', 1);
+        }
+
+        if(!empty($tids)) {
+            $terms = Term::loadMultiple($tids);
+            foreach ($terms as $term) {
+                $tags_tree[] = ['tid' => $term->id(),'name' => $term->getName()];
+            }
+        }
 
         return array(
                 '#theme' => 'tags_block',
